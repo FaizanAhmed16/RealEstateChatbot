7623822ba4b54009ec9bbf7d6a0601d003019c2b
Faizan Ahmed <105942072+FaizanAhmed16@users.noreply.github.com>
2024-07-08 12:12:41 +0500

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,65 @@
+import random
+import json
+
+import torch
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+gptj_model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B")
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+
+def get_gptj_response(user_query):
+    encoded_input = tokenizer(user_query, return_tensors="pt")
+    output = gptj_model.generate(**encoded_input, max_length=500,  # Adjust max_length as needed
+                                do_sample=True, top_k=50, top_p=0.9)  # Control response generation
+
+bot_name = "Sam"
+print("Let's chat! (type 'quit' to exit)")
+while True:
+    # sentence = "do you use credit cards?"
+    sentence = input("You: ")
+    if sentence == "quit":
+        break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+            gpt_response = get_gptj_response(sentence)  # Call the function to get GPT-J response
+            print(f"{bot_name}: {gpt_response}")
+
